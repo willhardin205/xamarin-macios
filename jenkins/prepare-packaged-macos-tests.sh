0a9dd8f08920ee169013e9880b9a85a72c3f3553
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-07-12 08:28:49 +0200

    [tests] Package Xamarin.Mac tests using 7z instead of zip.
    This makes the package 33% smaller (from 176mb to 117mb).

--- jenkins/prepare-packaged-macos-tests.sh
+++ jenkins/prepare-packaged-macos-tests.sh
@@ -52,15 +52,15 @@ fi
 
 env
 rm -f -- ./*.zip
-curl -fL "$URL" --output mac-test-package.zip
+curl -fL "$URL" --output mac-test-package.7z
 rm -rf mac-test-package
-unzip -o mac-test-package.zip
+7z x mac-test-package.7z
 cd mac-test-package
 
 COUNTER=0
 while [[ $COUNTER -lt 5 ]]; do
 	EC=0
-	./system-dependencies.sh --provision-mono --ignore-autotools --ignore-xamarin-studio --ignore-xcode --ignore-osx --ignore-cmake --ignore-simulators --ignore-7z || EC=$?
+	./system-dependencies.sh --provision-mono --ignore-autotools --ignore-xamarin-studio --ignore-xcode --ignore-osx --ignore-cmake --ignore-simulators --provision-7z || EC=$?
 	if [[ $EC -eq 56 ]]; then
 		# Sometimes we get spurious "curl: (56) SSLRead() return error -9806" errors. Trying again usually works, so lets try again a few more times.
 		# https://github.com/xamarin/maccore/issues/1098
